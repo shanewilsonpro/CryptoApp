1d6e11fa38512a1e7e53fa870632ec4cdb5fce57
Shane Wilson <shanewilsonpro@gmail.com>
2021-09-29 15:47:09 -0700

    created rankings screen for ui

--- src/screens/RankingScreen/index.tsx
+++ src/screens/RankingScreen/index.tsx
@@ -1,10 +1,48 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, FlatList } from "react-native";
+import UserRankingItem from "../../components/UserRankingItem";
+import styles from "./styles";
+
+const image = require("../../../assets/images/Saly-20.png");
+
+const portfolioCoins = [
+  {
+    id: "1",
+    name: "Virtual Dollars",
+    image: "https://g.foolcdn.com/art/companylogos/square/btc.png",
+    netWorth: 56440,
+  },
+  {
+    id: "2",
+    name: "Bitcoin",
+    image: "https://g.foolcdn.com/art/companylogos/square/btc.png",
+    netWorth: 36440,
+  },
+  {
+    id: "3",
+    name: "Etherium",
+    image: "https://g.foolcdn.com/art/companylogos/square/btc.png",
+    netWorth: 30120,
+  },
+
+];
 
 const RankingScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen component</Text>
+    <View style={styles.root}>
+      <FlatList
+        style={{ width: "100%" }}
+        data={portfolioCoins}
+        renderItem={({ item, index }) => <UserRankingItem user={item} place={index + 1} />}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponentStyle={{alignItems: "center"}}
+        ListHeaderComponent={() => (
+          <>
+            <Image style={styles.image} source={image} />
+            <Text style={styles.label}>Rankings</Text>
+          </>
+        )}
+      />
     </View>
   );
 };
