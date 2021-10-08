549e676497ad075eb1de9edfdf423eec041aea55
Shane Wilson <shanewilsonpro@gmail.com>
2021-10-08 12:15:37 -0700

    fetch portfolio data

--- src/screens/PortfolioScreen/index.tsx
+++ src/screens/PortfolioScreen/index.tsx
@@ -1,14 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { View, Text, Image, FlatList } from "react-native";
-import { API, graphql } from 'aws-amplify';
-
+import { API, graphqlOperation } from 'aws-amplify';
+import { getUserPortfolio } from "./queries";
 import PortfolioCoin from "../../components/PortfolioCoin";
 import styles from "./styles";
+import AppContext from "../../utilis/AppContext";
 
 const image = require("../../../assets/images/Saly-10.png");
 
 const PortfolioScreen = () => {
+  const [balance, setBalance] = useState(0);
   const [portfolioCoins, setPortfolioCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const { userId } = useContext(AppContext);
+
+  const fetchPortfolio = async () => {
+    setLoading(true);
+    try {
+      const response = await API.graphql(
+        graphqlOperation(
+          getUserPortfolio,
+          { id: userId },
+        )
+      )
+      console.log(response.data.getUser);
+      setBalance(response.data.getUser.networth);
+      setPortfolioCoins(response.data.getUser.portfolioCoins.items);
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchPortfolio();
+  }, [])
 
   return (
     <View style={styles.root}>
@@ -16,6 +44,8 @@ const PortfolioScreen = () => {
         style={{ width: "100%" }}
         data={portfolioCoins}
         renderItem={({ item }) => <PortfolioCoin portfolioCoin={item} />}
+        onRefresh={fetchPortfolio}
+        refreshing={loading}
         showsVerticalScrollIndicator={false}
         ListHeaderComponentStyle={{alignItems: "center"}}
         ListHeaderComponent={() => (
@@ -23,7 +53,7 @@ const PortfolioScreen = () => {
             <Image style={styles.image} source={image} />
             <View style={styles.balanceContainer}>
               <Text style={styles.label}>Portfolio balance</Text>
-              <Text style={styles.balance}>$56.440</Text>
+              <Text style={styles.balance}>${balance}</Text>
             </View>
           </>
         )}
